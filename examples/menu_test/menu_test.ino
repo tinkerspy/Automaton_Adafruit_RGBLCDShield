3d395d08fb671c0723bfe056f752ee16ffacfe1b
Tinkerspy <tinkerspy@myown.mailcan.com>
2016-07-02 22:15:47 +0200

    Chamed app.run() -> automaton.run(), dropped app.component()

--- examples/menu_test/menu_test.ino
+++ examples/menu_test/menu_test.ino
@@ -8,7 +8,6 @@
 Atm_led led1, led2, led3; 
 Atm_Menu lcd_display;
 Automaton_Adafruit_RGBLCDShield_Buttons lcd_buttons;
-Appliance app; 
 
 int pin1 = 4;
 int pin2 = 5;
@@ -16,27 +15,21 @@ int pin3 = 6;
 
 void setup() {
   
-  app.component( 
-    led1.begin( pin1 )
-      .blink( 500 )
-      .trigger( led1.EVT_BLINK ) 
-  );
-  app.component( 
-    led2.begin( pin2 )
-      .blink( 500 )
-      .trigger( led2.EVT_BLINK )
-  );
-  app.component( 
-    led3.begin( pin3 )
-      .blink( 500 )
-      .trigger( led3.EVT_BLINK )
-  );
-  app.component( lcd_display.begin( ) );
-  app.component( lcd_buttons.begin( lcd_display ) ); // Link the buttons to the display
+  led1.begin( pin1 )
+    .blink( 500 )
+    .trigger( led1.EVT_BLINK );
+  led2.begin( pin2 )
+    .blink( 500 )
+    .trigger( led2.EVT_BLINK );
+  led3.begin( pin3 )
+    .blink( 500 )
+    .trigger( led3.EVT_BLINK );
+  lcd_display.begin( );
+  lcd_buttons.begin( lcd_display ); // Link the buttons to the display
 
 }
 
 void loop() {
-  app.run();
+  automaton.run();
 }
 
